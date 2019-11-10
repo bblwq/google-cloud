67a6802ffe960559980d490c92b7a6dcf00b688b
Weiqi Liu <bblwq@users.noreply.github.com>
2019-11-09 23:46:33 -0500

    Update MyErrorController.java

--- src/main/java/com/bblwq/home/MyErrorController.java
+++ src/main/java/com/bblwq/home/MyErrorController.java
@@ -2,12 +2,12 @@ package com.bblwq.home;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MyErrorController implements ErrorController {
 
-    @GetMapping(value = "/error")
+    @RequestMapping("/error")
     public String handleError() {
         return "error";
     }
